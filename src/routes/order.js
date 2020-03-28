84b189341597838cf6e4bf7ce809c3e23316e4f8
adiamahardika <adiamahardika.work@gmail.com>
2020-03-28 21:28:03 +0700

    add order product

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,21 +1,21 @@
-// const express = require('express')
-// const Route = express.Router()
-
-// const { insertOrder, getAllOrder } = require('../controllers/order')
-
-// Route
-//   .post('/', insertOrder)
-//   .get('/', getAllOrder)
-//   .get('/:product_id', getAllOrder)
-
-// module.exports = Route
 const express = require('express')
-const route = express.Router()
+const Route = express.Router()
 
-const { insertOrder, newInsertOrder } = require('../controllers/order')
+const { orderProduct, readOrder } = require('../controllers/order')
 
-route
-    // .post('/', insertOrder)
-    .post('/', newInsertOrder)
+Route
+  .post('/', orderProduct)
+  .get('/', readOrder)
+//   .get('/:product_id', readOrder)
 
-module.exports = route
+module.exports = Route
+// const express = require('express')
+// const route = express.Router()
+
+// const { insertOrder, newInsertOrder } = require('../controllers/order')
+
+// route
+//     // .post('/', insertOrder)
+//     .post('/', newInsertOrder)
+
+// module.exports = route
